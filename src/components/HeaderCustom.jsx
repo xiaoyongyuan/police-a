dff160e2fc8d23f27b25446c19fbaf4257fd8b0e
mikeJang <mikejang999@163.com>
2019-04-15 17:47:28 +0800

    jhy

--- src/components/HeaderCustom.jsx
+++ src/components/HeaderCustom.jsx
@@ -1,13 +1,13 @@
 /**
  * 头部登录人信息
  */
-import React, { Component } from 'react';
-import { Menu, Icon, Layout} from 'antd';
-import screenfull from 'screenfull';
-import icon_admin from '../style/imgs/icon_admin.png';
-import icon_user from '../style/imgs/icon_user.png';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { Menu, Icon, Layout } from "antd";
+import screenfull from "screenfull";
+import icon_admin from "../style/imgs/icon_admin.png";
+import icon_user from "../style/imgs/icon_user.png";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import logo from "../style/jhy/imgs/logo.png";
 
 const { Header } = Layout;
@@ -15,100 +15,141 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 class HeaderCustom extends Component {
-    state = {
-        user: '',
-        visible: false,
-    };
-    componentDidMount() {
-        const _user = JSON.parse(localStorage.getItem('policeuser'));
-        if(!_user){
-            this.props.history.push('/login');
-        }else{
-            this.setState({
-                user: _user
-            });
-            this.notify(_user.account)
-        }
-    };
-    screenFull = () => { //全屏
-        screenfull.toggle();
-        this.props.toggle();
-    };
-    menuClick = e => {
-        e.key === 'logout' && this.logout();
-    };
-    logout = () => { //退出
-        localStorage.removeItem('policeuser');
-        localStorage.removeItem('policetoken');
-        this.props.history.push('/login')
-    };
-    popoverHide = () => {
-        this.setState({
-            visible: false,
-        });
-    };
-    handleVisibleChange = (visible) => {
-        this.setState({ visible });
-    };
-    namequf=()=>{
-        const usertype=this.state.user?this.state.user.usertype:null;
-        if(usertype){
-          switch(usertype){
-            case '0':
-            return '公安厅';
-            case '1':
-            return '公安局';
-            case '2':
-            return '公安分局';
-        }
-        }
-
+  state = {
+    user: "",
+    visible: false
+  };
+  componentDidMount() {
+    const _user = JSON.parse(localStorage.getItem("policeuser"));
+    if (!_user) {
+      this.props.history.push("/login");
+    } else {
+      this.setState({
+        user: _user
+      });
+      this.notify(_user.account);
     }
-    notify=(account)=>{ //消息推送
-        window.pushJs.init({
-           appKey:'7FD0C3504B625DE9219B5808B076D668',
-           chanel:'13152426570'
-        }).connect();
-        window.pushJs.onmessage = function(message) {
-            console.log("receive:" + message.data);
-        }
-        window.pushJs.onopen = function() {
-            console.log('open');
-        };
-
+  }
+  screenFull = () => {
+    //全屏
+    screenfull.toggle();
+    this.props.toggle();
+  };
+  menuClick = e => {
+    e.key === "logout" && this.logout();
+  };
+  logout = () => {
+    //退出
+    localStorage.removeItem("policeuser");
+    localStorage.removeItem("policetoken");
+    this.props.history.push("/login");
+  };
+  popoverHide = () => {
+    this.setState({
+      visible: false
+    });
+  };
+  handleVisibleChange = visible => {
+    this.setState({ visible });
+  };
+  namequf = () => {
+    const usertype = this.state.user ? this.state.user.usertype : null;
+    if (usertype) {
+      switch (usertype) {
+        case "0":
+          return "公安厅";
+        case "1":
+          return "公安局";
+        case "2":
+          return "公安分局";
+        default:
+          return "";
+      }
     }
-    render() {
-        return (
-            <Header className="custom-theme header" style={{display:'flex', justifyContent:'space-between'}}>
-                <div className="logotext" style={{height:'65px',width:'500px'}}>
-                    <div style={{padding:'15px 0 0 15px'}}>
-                    <img src={logo} width="33px" style={{verticalAlign:'top'}} />
-                    <span style={{lineHeight:'35px',height:'35px',verticalAlign:'top',display:'inlineBlock',paddingLeft:'12px',fontSize:'25px',fontFamily:'黑体',color:'#fff' }}>{this.state.user?this.state.user.zonename:null}{this.namequf()}</span>
-                    </div>
-                </div>
-                <Menu
-                    mode="horizontal"
-                    style={{ lineHeight: '64px', float: 'right' }}
-                    onClick={this.menuClick}
-                >
-                    <Menu.Item key="full" onClick={this.screenFull} >
-                        <Icon type="arrows-alt" onClick={this.screenFull} />
-                    </Menu.Item>
-                    <SubMenu title={<span className="avatar"><img src={!this.props.user.userstatus?icon_user:icon_admin}  alt="头像" /><i className="on bottom b-white" /></span>}>
-                        <MenuItemGroup title="用户中心">
-                            <Menu.Item key="setting:1">你好 - {this.props.user.account}</Menu.Item>
-                            <Menu.Item key="logout"><span onClick={this.logout}>退出登录</span></Menu.Item>
-                        </MenuItemGroup>
-                    </SubMenu>
-                </Menu>
-            </Header>
-        )
-    }
+  };
+  notify = account => {
+    //消息推送
+    window.pushJs
+      .init({
+        appKey: "7FD0C3504B625DE9219B5808B076D668",
+        chanel: "13152426570"
+      })
+      .connect();
+    window.pushJs.onmessage = function(message) {
+      console.log("receive:" + message.data);
+    };
+    window.pushJs.onopen = function() {
+      console.log("open");
+    };
+  };
+  render() {
+    return (
+      <Header
+        className="custom-theme header"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <div className="logotext" style={{ height: "65px", width: "500px" }}>
+          <div style={{ padding: "15px 0 0 15px" }}>
+            <img
+              src={logo}
+              width="33px"
+              style={{ verticalAlign: "top" }}
+              alt=""
+            />
+            <span
+              style={{
+                lineHeight: "35px",
+                height: "35px",
+                verticalAlign: "top",
+                display: "inlineBlock",
+                paddingLeft: "12px",
+                fontSize: "25px",
+                fontFamily: "黑体",
+                color: "#fff"
+              }}
+            >
+              {this.state.user ? this.state.user.zonename : null}
+              {this.namequf()}
+            </span>
+          </div>
+        </div>
+        <Menu
+          mode="horizontal"
+          style={{ lineHeight: "64px", float: "right" }}
+          onClick={this.menuClick}
+        >
+          <Menu.Item key="full" onClick={this.screenFull}>
+            <Icon type="arrows-alt" onClick={this.screenFull} />
+          </Menu.Item>
+          <SubMenu
+            title={
+              <span className="avatar">
+                <img
+                  src={!this.props.user.userstatus ? icon_user : icon_admin}
+                  alt="头像"
+                />
+                <i className="on bottom b-white" />
+              </span>
+            }
+          >
+            <MenuItemGroup title="用户中心">
+              <Menu.Item key="setting:1">
+                你好 - {this.props.user.account}
+              </Menu.Item>
+              <Menu.Item key="logout">
+                <span onClick={this.logout}>退出登录</span>
+              </Menu.Item>
+            </MenuItemGroup>
+          </SubMenu>
+        </Menu>
+      </Header>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const { responsive = {data: {}} } = state.httpData;
-    return {responsive};
+  const { responsive = { data: {} } } = state.httpData;
+  return { responsive };
 };
 
 export default withRouter(connect(mapStateToProps)(HeaderCustom));
--- src/components/alarm/AlarmList.js
+++ src/components/alarm/AlarmList.js
@@ -1,230 +1,325 @@
-import React, { Component } from 'react';
-import {Form, DatePicker, Row, Col, Button,LocaleProvider,Modal,Icon,Pagination,Spin} from 'antd';
-import {post} from "../../axios/tools";
-import zh_CN from 'antd/lib/locale-provider/zh_CN';
-import 'moment/locale/zh-cn';
-import '../../style/sjg/police.css';
+import React, { Component } from "react";
+import {
+  Form,
+  DatePicker,
+  Row,
+  Col,
+  Button,
+  LocaleProvider,
+  Modal,
+  Icon,
+  Pagination,
+  Spin
+} from "antd";
+import { post } from "../../axios/tools";
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+import "moment/locale/zh-cn";
+import "../../style/sjg/police.css";
 import nodata from "../../style/imgs/nodata.png";
 import AlarmDetail from "./AlarmDetail";
 const FormItem = Form.Item;
 const RangePicker = DatePicker.RangePicker;
 class AlarmList extends Component {
-    constructor(props){
-        super(props);
-        this.state= {
-            alarmImgType: false,
-            callPoliceList: [],
-            page:1,
-            spinStyle:true
-        }
-    }
-    componentDidMount(){
-        this.callPolice();
-    }
-    callPolice=()=>{
-        var datas={
-            pagesize:10,
-            pageindex:this.state.page,
-            bdate:this.state.bdate,
-            edate:this.state.edate,
-        };
-        post({url:"/api/alarmhandle_cop/getlist",data:datas},(res)=>{
-            if(res.success){
-                this.setState({
-                    callPoliceList:res.data,
-                    spinStyle:false,
-                    totalcount:res.totalcount,
-                })
-            }
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      alarmImgType: false,
+      callPoliceList: [],
+      page: 1,
+      spinStyle: true
     };
-    selectopt = (e) => { //检索
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if(!err){
-                this.setState({
-                    spinStyle:true,
-                    bdate:values.range_picker1&&values.range_picker1.length?values.range_picker1[0].format("YYYY-MM-DD"):"",
-                    edate:values.range_picker1&&values.range_picker1.length?values.range_picker1[1].format("YYYY-MM-DD"):""
-                },()=>{
-                    this.callPolice();
-                })
-            }
-        })
+  }
+  componentDidMount() {
+    this.callPolice();
+  }
+  callPolice = () => {
+    var datas = {
+      pagesize: 10,
+      pageindex: this.state.page,
+      bdate: this.state.bdate,
+      edate: this.state.edate
     };
-    progress=(astatus)=>{
-        if(astatus===0){
-            return "已推送未处理";
-        }else if(astatus===1){
-            return "已接警";
-        }else if(astatus===2){
-            return "处理中";
-        }else if(astatus===3){
-            return "已结束";
-        }
-    };
-    handleCancelAlarmImg =()=>{
+    post({ url: "/api/alarmhandle_cop/getlist", data: datas }, res => {
+      if (res.success) {
         this.setState({
-            alarmImgType:false
-        })
-    };
-    //分页
-    handlepage=(page)=>{
-        this.setState({
-            page,
-            spinStyle:true
-        },()=>{
+          callPoliceList: res.data,
+          spinStyle: false,
+          totalcount: res.totalcount
+        });
+      }
+    });
+  };
+  selectopt = e => {
+    //检索
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.setState(
+          {
+            spinStyle: true,
+            bdate:
+              values.range_picker1 && values.range_picker1.length
+                ? values.range_picker1[0].format("YYYY-MM-DD")
+                : "",
+            edate:
+              values.range_picker1 && values.range_picker1.length
+                ? values.range_picker1[1].format("YYYY-MM-DD")
+                : ""
+          },
+          () => {
             this.callPolice();
-        })
-    };
-    handleState=(state,atime,adminname,lastmemo,lastmen)=> {
-        if (state === 0) {
-            return (
-                <div className="dashed">
-                    <Row className="pol polone">
-                        <Col span={24}><span className="powercolor">已推送</span></Col>
-                    </Row>
-                    <Row className="pol poltwo">
-                        <Col span={24}><span className="powercolor">{atime}</span></Col>
-                    </Row>
-                </div>
-            )
-        } else if(state===1) {
-            return(
-                <div className="dashed">
-                    <Row className="pol polone">
-                        <Col span={24}><span className="powercolor">接警人：</span>{adminname}</Col>
-                    </Row>
-                    <Row className="pol poltwo">
-                        <Col span={24}><span className="powercolor">最新进展：</span>{lastmemo}&nbsp;&nbsp;{atime}</Col>
-                    </Row>
-                </div>
-            )
-        }else if(state===2){
-            return(
-                <div className="dashed">
-                    <Row className="pol polone">
-                        <Col span={24}><span className="powercolor">处理人：</span>{lastmen}</Col>
-                    </Row>
-                    <Row className="pol poltwo">
-                        <Col span={24}><span className="powercolor">最新进展：</span>{lastmemo}&nbsp;&nbsp;{atime}</Col>
-                    </Row>
-                </div>
-            )
-        }else if(state===3){
-            return(
-                <div className="dashed">
-                    <Row className="pol polone">
-                        <Col span={24}><span className="powercolor">已结案</span></Col>
-                    </Row>
-                    <Row className="pol poltwo">
-                        <Col span={24}><span className="powercolor">{atime}</span></Col>
-                    </Row>
-                </div>
-            )
-        }
-    };
-    detail=(astatus)=>{
-        if(astatus===0){
-            return "pushed";
-        }else if(astatus===1){
-            return "processing";
-        }else if(astatus===2){
-            return "alarmReceiver";
-        }else if(astatus===3){
-            return "caseClosed";
-        }
-    };
-    linepolice=(astatus)=>{
-        if(astatus===0){
-            return "pushed policeline";
-        }else if(astatus===1){
-            return "processing policeline";
-        }else if(astatus===2){
-            return "alarmReceiver policeline";
-        }else if(astatus===3){
-            return "caseClosed policeline";
-        }
+          }
+        );
+      }
+    });
+  };
+  progress = astatus => {
+    if (astatus === 0) {
+      return "已推送未处理";
+    } else if (astatus === 1) {
+      return "已接警";
+    } else if (astatus === 2) {
+      return "处理中";
+    } else if (astatus === 3) {
+      return "已结束";
     }
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <div className="AlarmList">
-                <div className="shange">
-                    <Row style={{margin:'1%'}}>
-                        <Col span={18}>
-                            <LocaleProvider locale={zh_CN}>
-                                <Form layout="inline" onSubmit={this.selectopt}>
-                                    <Form.Item
-                                        label="日期"
-                                    >
-                                        {getFieldDecorator('range_picker1')(
-                                            <RangePicker placeholder={['开始时间', '结束时间']} />
-                                        )}
-                                    </Form.Item>
-                                    <FormItem>
-                                        <Button type="primary" htmlType="submit">
-                                            查询
-                                        </Button>
-                                    </FormItem>
-                                </Form>
-                            </LocaleProvider>
-                        </Col>
-                    </Row>
-                </div>
-                <div style={{width:"100%",height:"auto",textAlign:"center"}}><img src={nodata} alt="" style={{display:this.state.callPoliceList.length>0?"none":"inline-block",marginTop:"2%",width:"6%"}} /></div>
-                <Spin size="large" spinning={this.state.spinStyle}>
-                    <div>
-                        {
-                            this.state.callPoliceList.map((v,i)=>(
-                                <div className="policeboy" key={i}>
-                                    <a href={"#/app/alarm/AlarmDetail?id="+v.code} className="underline">
-                                        <div className="policeyuan">
-                                            <div className={this.detail(v.astatus)}>{i+1}</div>
-                                        </div>
-                                        <div className="policelist">
-                                            <div className={this.linepolice(v.astatus)}></div>
-                                            <div className="policecon">
-                                                <div className="policeinf">
-                                                    <div className="poltop">
-                                                        <Row className="pol polone">
-                                                            <Col span={4}>{v.atime}</Col>
-                                                            <Col span={7}>{v.location}</Col>
-                                                            <Col span={6}><span><Icon type="user-add" style={{color:"#2980F3"}} /> 报警人：</span><span>{v.adminname}</span></Col>
-                                                            <Col span={7}><span><Icon type="phone" style={{color:"#2980F3"}} className="iphone" /> 联系电话：</span><span>{v.adminaccount}</span></Col>
-                                                        </Row>
-                                                        <Row className="pol poltwo">
-                                                            <Col span={24}><span className="powercolor">警情描述：</span><span>{v.lastmemo?v.lastmemo:"无"}</span> </Col>
-                                                        </Row>
-                                                    </div>
-                                                    {this.handleState(v.astatus,v.atime,v.adminname,v.lastmemo,v.lastmen)}
-                                                </div>
-                                                <div className="policeimg">
-                                                    <img src={v.pic_min} alt="" />
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </a>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </Spin>
-                <div className="pagination" style={{display:this.state.callPoliceList?"block":"none"}}><Pagination defaultCurrent={1} current={this.state.page} total={this.state.totalcount} defaultPageSize={10} onChange={this.handlepage} hideOnSinglePage={true} /></div>
-                <Modal
-                    width={1000}
-                    title="警情详情"
-                    visible={this.state.alarmImgType}
-                    onCancel={this.handleCancelAlarmImg}
-                    footer={null}
+  };
+  handleCancelAlarmImg = () => {
+    this.setState({
+      alarmImgType: false
+    });
+  };
+  //分页
+  handlepage = page => {
+    this.setState(
+      {
+        page,
+        spinStyle: true
+      },
+      () => {
+        this.callPolice();
+      }
+    );
+  };
+  handleState = (state, atime, adminname, lastmemo, lastmen) => {
+    if (state === 0) {
+      return (
+        <div className="dashed">
+          <Row className="pol polone">
+            <Col span={24}>
+              <span className="powercolor">已推送</span>
+            </Col>
+          </Row>
+          <Row className="pol poltwo">
+            <Col span={24}>
+              <span className="powercolor">{atime}</span>
+            </Col>
+          </Row>
+        </div>
+      );
+    } else if (state === 1) {
+      return (
+        <div className="dashed">
+          <Row className="pol polone">
+            <Col span={24}>
+              <span className="powercolor">接警人：</span>
+              {adminname}
+            </Col>
+          </Row>
+          <Row className="pol poltwo">
+            <Col span={24}>
+              <span className="powercolor">最新进展：</span>
+              {lastmemo}&nbsp;&nbsp;{atime}
+            </Col>
+          </Row>
+        </div>
+      );
+    } else if (state === 2) {
+      return (
+        <div className="dashed">
+          <Row className="pol polone">
+            <Col span={24}>
+              <span className="powercolor">处理人：</span>
+              {lastmen}
+            </Col>
+          </Row>
+          <Row className="pol poltwo">
+            <Col span={24}>
+              <span className="powercolor">最新进展：</span>
+              {lastmemo}&nbsp;&nbsp;{atime}
+            </Col>
+          </Row>
+        </div>
+      );
+    } else if (state === 3) {
+      return (
+        <div className="dashed">
+          <Row className="pol polone">
+            <Col span={24}>
+              <span className="powercolor">已结案</span>
+            </Col>
+          </Row>
+          <Row className="pol poltwo">
+            <Col span={24}>
+              <span className="powercolor">{atime}</span>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+  };
+  detail = astatus => {
+    if (astatus === 0) {
+      return "pushed";
+    } else if (astatus === 1) {
+      return "processing";
+    } else if (astatus === 2) {
+      return "alarmReceiver";
+    } else if (astatus === 3) {
+      return "caseClosed";
+    }
+  };
+  linepolice = astatus => {
+    if (astatus === 0) {
+      return "pushed policeline";
+    } else if (astatus === 1) {
+      return "processing policeline";
+    } else if (astatus === 2) {
+      return "alarmReceiver policeline";
+    } else if (astatus === 3) {
+      return "caseClosed policeline";
+    }
+  };
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="AlarmList">
+        <div className="shange">
+          <Row style={{ margin: "1%" }}>
+            <Col span={18}>
+              <LocaleProvider locale={zh_CN}>
+                <Form layout="inline" onSubmit={this.selectopt}>
+                  <Form.Item label="日期">
+                    {getFieldDecorator("range_picker1")(
+                      <RangePicker placeholder={["开始时间", "结束时间"]} />
+                    )}
+                  </Form.Item>
+                  <FormItem>
+                    <Button type="primary" htmlType="submit">
+                      查询
+                    </Button>
+                  </FormItem>
+                </Form>
+              </LocaleProvider>
+            </Col>
+          </Row>
+        </div>
+        <div style={{ width: "100%", height: "auto", textAlign: "center" }}>
+          <img
+            src={nodata}
+            alt=""
+            style={{
+              display:
+                this.state.callPoliceList.length > 0 ? "none" : "inline-block",
+              marginTop: "2%",
+              width: "6%"
+            }}
+          />
+        </div>
+        <Spin size="large" spinning={this.state.spinStyle}>
+          <div>
+            {this.state.callPoliceList.map((v, i) => (
+              <div className="policeboy" key={i}>
+                <a
+                  href={"#/app/alarm/AlarmDetail?id=" + v.code}
+                  className="underline"
                 >
-                    <AlarmDetail visible={this.state.alarmImgType} toson={this.state.toson} />
-                </Modal>
-
-            </div>
-        )
-    }
+                  <div className="policeyuan">
+                    <div className={this.detail(v.astatus)}>{i + 1}</div>
+                  </div>
+                  <div className="policelist">
+                    <div className={this.linepolice(v.astatus)} />
+                    <div className="policecon">
+                      <div className="policeinf">
+                        <div className="poltop">
+                          <Row className="pol polone">
+                            <Col span={4}>{v.atime}</Col>
+                            <Col span={7}>{v.location}</Col>
+                            <Col span={6}>
+                              <span>
+                                <Icon
+                                  type="user-add"
+                                  style={{ color: "#2980F3" }}
+                                />{" "}
+                                报警人：
+                              </span>
+                              <span>{v.adminname}</span>
+                            </Col>
+                            <Col span={7}>
+                              <span>
+                                <Icon
+                                  type="phone"
+                                  style={{ color: "#2980F3" }}
+                                  className="iphone"
+                                />{" "}
+                                联系电话：
+                              </span>
+                              <span>{v.adminaccount}</span>
+                            </Col>
+                          </Row>
+                          <Row className="pol poltwo">
+                            <Col span={24}>
+                              <span className="powercolor">警情描述：</span>
+                              <span>{v.lastmemo ? v.lastmemo : "无"}</span>{" "}
+                            </Col>
+                          </Row>
+                        </div>
+                        {this.handleState(
+                          v.astatus,
+                          v.atime,
+                          v.adminname,
+                          v.lastmemo,
+                          v.lastmen
+                        )}
+                      </div>
+                      <div className="policeimg">
+                        <img src={v.pic_min} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+        </Spin>
+        <div
+          className="pagination"
+          style={{ display: this.state.callPoliceList ? "block" : "none" }}
+        >
+          <Pagination
+            defaultCurrent={1}
+            current={this.state.page}
+            total={this.state.totalcount}
+            defaultPageSize={10}
+            onChange={this.handlepage}
+            hideOnSinglePage={true}
+          />
+        </div>
+        <Modal
+          width={1000}
+          title="警情详情"
+          visible={this.state.alarmImgType}
+          onCancel={this.handleCancelAlarmImg}
+          footer={null}
+        >
+          <AlarmDetail
+            visible={this.state.alarmImgType}
+            toson={this.state.toson}
+          />
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default AlarmList=Form.create()(AlarmList);
+export default (AlarmList = Form.create()(AlarmList));
